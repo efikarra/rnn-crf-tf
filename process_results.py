57a00a5beba9c0f5a70f970d43582ceada342760
Efi <efi@dhcp-v028-089.mobile.uci.edu>
2018-03-14 13:11:20 -0700

    rm sns

--- process_results.py
+++ process_results.py
@@ -2,7 +2,7 @@ from sklearn.metrics import classification_report, confusion_matrix, roc_auc_sco
 import numpy as np
 import pandas as pd
 import matplotlib
-import seaborn as sns
+# import seaborn as sns
 # matplotlib.rcParams['font.sans-serif'] = 'SimHei'
 # matplotlib.rcParams['font.serif'] = 'SimHei'
 # matplotlib.rcParams['font.family'] = "sans-serif"
@@ -24,14 +24,14 @@ def compute_classification_report(predictions, targets):
     conf_matrix = confusion_matrix(targets, predictions)
     print "\nConfusion matrix:\n", conf_matrix
     print(classification_report(targets, predictions))
-    df_cm = pd.DataFrame(conf_matrix)
-                         # index=target_names,
-    #                      columns=target_names)
-    plt.figure(figsize=(10, 7))
-    sns.heatmap(df_cm, annot=True)
-    plt.ylabel('Actual')
-    plt.xlabel('Predicted')
-    plt.show()
+    # df_cm = pd.DataFrame(conf_matrix)
+    #                      # index=target_names,
+    # #                      columns=target_names)
+    # plt.figure(figsize=(10, 7))
+    # sns.heatmap(df_cm, annot=True)
+    # plt.ylabel('Actual')
+    # plt.xlabel('Predicted')
+    # plt.show()
 
 def labels_from_predictions(predictions):
     return np.argmax(predictions, axis=1)
